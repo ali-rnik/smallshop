fc0e42f57ca9bc2dd1c1b64257bf5cf8d9464e1d
Ali R. Nik <arnik@tutanota.com>
2021-06-02 21:20:13 +0430

    delete and modify diesel-cli auto generated files

--- src/schema.rs
+++ src/schema.rs
@@ -11,15 +11,3 @@ table! {
     }
 }
 
-table! {
-    users (id) {
-        id -> Int4,
-        name -> Text,
-        password -> Text,
-    }
-}
-
-allow_tables_to_appear_in_same_query!(
-    products,
-    users,
-);
