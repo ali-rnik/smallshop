ce42512f2aec9d74a2a20644020e9d279ce04320
Ali R. Nik <arnik@tutanota.com>
2021-06-03 14:08:12 +0430

    add delete feature to db

--- src/store.rs
+++ src/store.rs
@@ -57,10 +57,11 @@ async fn store_list_items(_user: login::User,
 			  db: Db)
     -> Template
 {
-    let table: Vec<(String, String)> = db.run(move |conn| {
+    let table: Vec<(Option<i32>, String, String)> = db.run(move |conn| {
 	schema::products::table
-	    .select((schema::products::product_name,
-		    schema::products::unit_price))
+	    .select((schema::products::product_id,
+		     schema::products::product_name,
+		     schema::products::unit_price))
 	    .load(conn)
     }).await.expect("Could not load id's from database");
 
@@ -71,8 +72,26 @@ async fn store_list_items(_user: login::User,
     Template::render("store_list-items", context)
 }
 
+#[get("/delete_item/<id>")]
+async fn store_delete_item(_user: login::User,
+			   db: Db, id: i32)
+    -> Flash<Redirect>
+{
+    db.run(move |conn| {
+	diesel::delete(schema::products::table)
+	    .filter(schema::products::product_id.eq(id))
+	    .execute(conn)
+    }).await.expect("Could not delete item");
+
+
+    Flash::success(Redirect::to(uri!("/store", store_list_items)),
+		   "Item Deleted")
+          
+}
+
     
 pub fn stage() -> Vec<rocket::Route> {
-    routes![store, store_add_item, store_add_item_submit, store_list_items]
+    routes![store, store_add_item, store_add_item_submit, store_list_items,
+	    store_delete_item]
 }
 
